839349aa1134a5a884ae8c47f77fdd73aea061eb
Ryan <zun0323@gmail.com>
2017-03-11 16:28:22 +0900

    Add objects in resumeBuilder.js Add project images

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,82 @@
-$("#main").append("Hellow World?]");
+var work = {
+  "jobs": [
+    {
+      "employer": "Alticast",
+      "title": "UI Designer",
+      "location": "KangNam, Seoul, South Korea",
+      "dates": "2003 - 2004",
+      "description": "Settlelite brodcasting company. Design romote control based user experience guide."
+    },
+    {
+      "employer": "Samsung Electronics",
+      "title": "Product Designer",
+      "location": "Seocho, Seoul, South Korea",
+      "dates":"2005 - ",
+      "description": "Grobal manufacturing company. Design mobile phone for 5years, design conceptual product since 2012."
+    }
+  ]
+}
+
+var projects = {
+  "projects": [
+    {
+      "title": "JM phone",
+      "dates": "2006",
+      "description": "coraboration work with JM.",
+      "images": ["./images/crop_billboard.jpg", "./images/crop_edm.jpg"]
+    },
+    {
+      "title": "Wearable project",
+      "dates": "2012",
+      "description": "coraboration work with fation part",
+      "images": ["./images/crop_miles.jpg"]
+    }
+  ]
+}
+
+var bio = {
+  "name": "Ryan",
+  "role": "Interative Developer",
+  "welcomeMessage": "Hellow World!",
+  "biopic": "./images/img_3495.jpg",
+  "contacts": {
+    "mobile": "010-4229-7858",
+    "email": "zun0323@gmail.com",
+    "github": "zun0323",
+    "location": "Seoul, SouthKorea"
+  },
+  "skills": ["HTML", "CSS", "JavaScript", "Python"]
+}
+
+var education = {
+  "schools": [
+    {
+      "name": "SNUT",
+      "location": "Seoul, SouthKorea",
+      "degreeDates": "2005",
+      "url": "http://www.snut.or.kr",
+      "majors": ["Inderstrial Designer"]
+    },
+    {
+      "name": "DesignMembership",
+      "location": "Seoul, SouthKorea",
+      "degreeDates": "2005",
+      "url": "http://www.designmembership.com",
+      "majors": ["Product Designer"]
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "Basic course",
+      "school": "Code School",
+      "dates": "2016",
+      "url": "http://www.codeschool.org"
+    },
+    {
+      "title": "Front-end Developer",
+      "school": "Udacity",
+      "dates": "2017",
+      "url": "http://www.ucacity.com"
+    }
+  ]
+}
