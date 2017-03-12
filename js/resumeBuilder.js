d291e9f86bfa1b357e4c811d3728dedf27b48db3
Ryan <zun0323@gmail.com>
2017-03-12 17:23:56 +0900

    Add some functions to resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -23,19 +23,19 @@ var projects = {
       "title": "JM phone",
       "dates": "2006",
       "description": "coraboration work with JM.",
-      "images": ["./images/crop_billboard.jpg", "./images/crop_edm.jpg"]
+      "images": ["./images/Crop_billboard.jpg", "./images/Crop_edm.jpg"]
     },
     {
       "title": "Wearable project",
       "dates": "2012",
       "description": "coraboration work with fation part",
-      "images": ["./images/crop_miles.jpg"]
+      "images": ["./images/Crop_miles.jpg"]
     }
   ]
 }
 
 var bio = {
-  "name": "Ryan",
+  "name": "ryan kwon",
   "role": "Interative Developer",
   "welcomeMessage": "Hellow World!",
   "biopic": "./images/img_3495.jpg",
@@ -80,3 +80,59 @@ var education = {
     }
   ]
 }
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").append(formattedName);
+
+if(bio.skills.length > 0) {
+  $("#header").append(HTMLskillsStart);
+  for(item in bio.skills) {
+    $("#skills").append(HTMLskills.replace("%data%", bio.skills[item]));
+  }
+}
+
+function displaywork() {
+  for(item in work.jobs) {
+    $("#workExperience").append(HTMLworkStart);
+
+    var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.jobs[item].employer);
+    var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[item].title);
+    var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[item].dates);
+    var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[item].description);
+
+    var formattedAllWorkData = formattedWorkEmployer + formattedWorkTitle + formattedWorkDates + formattedWorkDescription;
+
+    $(".work-entry:last").append(formattedAllWorkData);
+  }
+}
+
+displaywork();
+
+function inName(name) {
+  name.trim().split(" ");
+  name[1] = name[1].toUpperCase;
+  name[0] = name[0].slice(0,1).toUpperCase + name[0].slice(1).toLowerCase;
+  return name[0] + " " + name[1];
+}
+
+$("#main").append(internationalizeButton);
+
+projects.display = function() {
+  for(var i = 0; i < projects.projects.length; i++) {
+    $("#projects").append(HTMLprojectStart);
+
+    var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
+    var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
+    var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+    var formattedAllProjectData = formattedProjectTitle + formattedProjectDates + formattedProjectDescription;
+
+    for(var j = 0; j < projects.projects[i].images.length; j++) {
+      var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[i].images[j]);
+      formattedAllProjectData += formattedProjectImage;
+    }
+
+    $(".project-entry:last").append(formattedAllProjectData);
+  }
+}
+
+projects.display();
